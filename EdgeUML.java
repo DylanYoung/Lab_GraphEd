b3b1ff121a8cb747d2eab8eb4e35f0873eca7ac6
Dylan Young <dylanyoungmeijer@gmail.com>
2014-04-01 21:08:23 -0300

    UML edge working

--- EdgeUML.java
+++ EdgeUML.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 
@@ -9,6 +10,39 @@ public class EdgeUML extends AbstractEdge {
 	@Override
 	public void draw(Graphics2D g2) {
 		// TODO Auto-generated method stub
+
+		Line2D l = getConnectionPoints();
+		Point2D p1 = l.getP1();
+		Point2D p2 = l.getP2();
+		Double xDist = p1.getX() - p2.getX();
+		Double yDist = p1.getY() - p2.getY();
+		Double s2x, s2y, f2x, f2y;
+		
+		Point2D temp;
+		
+		if ( Math.abs(xDist) < Math.abs(yDist) )
+		{
+			s2x = p1.getX() - xDist/2;
+			s2y = p1.getY();
+			f2x = p1.getX() - xDist/2;
+			f2y = p2.getY(); 
+		}else{
+			s2x = p1.getX();
+			s2y = p2.getY() + yDist/2;
+			f2x = p2.getX();
+			f2y = p2.getY() + yDist/2; 
+		}
+				
+			
+		g2.draw( new Line2D.Double(
+				p1.getX(), p1.getY(), 
+				s2x, s2y) );
+		g2.draw( new Line2D.Double(
+				s2x, s2y, 
+				f2x, f2y) );
+		g2.draw( new Line2D.Double(
+				f2x, f2y, p2.getX(), p2.getY()
+				) );
 		
 	}
 
